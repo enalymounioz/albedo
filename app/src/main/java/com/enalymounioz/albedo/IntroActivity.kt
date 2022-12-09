ffc8a8146f9daa613cb5e85333de70fa4dd8eacd
Eleni Voreopoulou <evoreopoulou@wings-ict-solution.eu>
2022-12-09 17:29:46 +0200

    Initial commit

---
+++ app/src/main/java/com/enalymounioz/albedo/IntroActivity.kt
@@ -0,0 +1,24 @@
+package com.enalymounioz.albedo
+
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+}
